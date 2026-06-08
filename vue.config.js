0ac349ae743bd1838e0164947b3c499e8f4e7365
Vũ Anh Tuấn <tuanvuanh103@example.com>
2026-06-08 21:16:37 +0700

    Chuyen doi sang Vue CLI (Webpack) va cau hinh hoan tat

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    allowedHosts: 'all'
+  }
+});
